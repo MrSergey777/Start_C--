4f75f90b3321278f3dec52d8db71cb9502f31a59
MrSergey777 <mrsergey4476@gmail.com>
2025-01-03 16:43:56 +0300

    Add files via upload

---
+++ lab-18/18-2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+double sum(int n, ...) {
+    double* p = (double*)&n + 1;
+    double ym = 0;
+    double prev = *p; 
+    for (int i = 1; i < n; i++) {
+        double neew = *(++p); 
+        ym += prev * neew; 
+        prev = neew; 
+    }
+    return ym;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    cout << "Сумма для (1.5, 2.5, 3.5, 4.5): " << sum(4, 1.5, 2.5, 3.5, 4.5) << endl;
+    cout << "Сумма для (5.5, 6.5, 7.5): " << sum(3, 5.5, 6.5, 7.5) << endl;
+    cout << "Сумма для (8.5, 9.5): " << sum(2, 8.5, 9.5) << endl;
+    return 0;
+}
